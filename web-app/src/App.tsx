190bdb65af5b7ae3aa09144274910101988a831e
Italofd <italo@vendoo.co>
2025-02-28 21:42:37 -0300

    Initial commit =) i think we gonna have a good time here

---
+++ web-app/src/App.tsx
@@ -0,0 +1,14 @@
+import "tailwindcss";
+import "./globals.css";
+import { Teste } from "./teste";
+
+function App() {
+	return (
+		<>
+			<Teste />
+			<h1 className="text-5xl">{"This is Thess =)"}</h1>
+		</>
+	);
+}
+
+export default App;
---
+++ web-app/src/teste.tsx
@@ -0,0 +1,7 @@
+export const Teste = () => {
+	return (
+		<div>
+			<h2 className="text-5xl">Sei la meu mano</h2>
+		</div>
+	);
+};
